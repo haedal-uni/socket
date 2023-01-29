b14788f3f8e9ef3bbfeb172db8d256ea91c8e5d3
soyeon <luora@naver.com>
2023-01-30 05:42:56 +0900

    [add] exception 처리 [#5]

---
+++ src/main/java/com/dalcho/adme/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.dalcho.adme.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+	SOCKET_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅방을 만들어주세요."),
+	DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 에러");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	ErrorCode(HttpStatus httpStatus, String message) {
+		this.httpStatus = httpStatus;
+		this.message = message;
+	}
+}
+
