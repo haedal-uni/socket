bcc6e1a63533a97c0d65aefe2332c3bf2531bee5
soyeon <luora@naver.com>
2023-02-20 22:57:25 +0900

    [add] ThreadPoolTaskExecutor 설정 추가 [#18]

--- src/main/java/com/dalcho/adme/config/AsyncConfiguration.java
+++ src/main/java/com/dalcho/adme/config/AsyncConfiguration.java
@@ -6,27 +6,27 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
-@Configuration
-@EnableAsync
-public class AsyncConfiguration { // Async 설정
-	@Bean
+@Configuration // Bean 등록
+@EnableAsync // Async 설정
+public class AsyncConfiguration {
+	@Bean(name="executor")
 	public Executor asyncThreadPool() {
 		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-
-		taskExecutor.setCorePoolSize(3);
-		taskExecutor.setMaxPoolSize(30);
-		taskExecutor.setQueueCapacity(10);
+		taskExecutor.setCorePoolSize(3); // 생성할 개수(thread pool에 항상 존재하는 최소 개수)
+		taskExecutor.setMaxPoolSize(10); // 동시 동작하는 최대 Thread의 수
+		taskExecutor.setQueueCapacity(15); // 큐의 사이즈
 		taskExecutor.setThreadNamePrefix("Async-Executor-");
 		taskExecutor.setDaemon(true);
+		taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+		taskExecutor.setAwaitTerminationSeconds(60);
 		taskExecutor.initialize();
 
 		return taskExecutor;
 	}
 }
 /*
-기본적으로 Spring Event 는 동기적이다. 하지만 @Async 를 통해 비동기로 동작할 수 있다.
-Executor 쓰레드풀을 사용
-
 *@TransactionalEventListener
  */
