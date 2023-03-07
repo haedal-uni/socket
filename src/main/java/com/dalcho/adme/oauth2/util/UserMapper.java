110c1b5c907d83339ea509b1e148cfa01fe7c7e4
soyeon <luora@naver.com>
2023-03-08 00:27:46 +0900

    [delete] pw 삭제 [#27]

--- src/main/java/com/dalcho/adme/oauth2/util/UserMapper.java
+++ src/main/java/com/dalcho/adme/oauth2/util/UserMapper.java
@@ -1,31 +1,23 @@
 package com.dalcho.adme.oauth2.util;
 
-import com.dalcho.adme.model.Kakao;
+import com.dalcho.adme.model.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserMapper {
 
-	public static Kakao of(OAuth2User oAuth2User) {
+	public static User of(OAuth2User oAuth2User) {
 		var attributes = oAuth2User.getAttributes();
-		return Kakao.builder()
+		log.info("UserMapper의 of() " + attributes);
+		return User.builder()
 				.id((Long) attributes.get("id"))
 				.email((String) attributes.get("email"))
 				.password("")
-				.nickname((String) attributes.get("sub"))
-//                .picture((String)attributes.get("picture"))
-				.build();
-	}
-
-	public static Kakao toEntity(OAuth2User oAuth2User, String passwrod) {
-		var attributes = oAuth2User.getAttributes();
-		return Kakao.builder()
-				.id((Long) attributes.get("id"))
-				.email((String) attributes.get("email"))
-				.password(passwrod)
 				.nickname((String) attributes.get("sub"))
 //                .picture((String)attributes.get("picture"))
 				.build();
