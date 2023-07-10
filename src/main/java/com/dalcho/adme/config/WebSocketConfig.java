d71dd4cfceaace4b331b3af2b96c17472d25f2a0
soyeon <luora@naver.com>
2023-07-11 00:01:06 +0900

    [add] broker 설정 [#78]

--- src/main/java/com/dalcho/adme/config/WebSocketConfig.java
+++ src/main/java/com/dalcho/adme/config/WebSocketConfig.java
@@ -1,7 +1,11 @@
 package com.dalcho.adme.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,11 +18,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	private final StompHandler stompHandler;
-
+	private final RedisConnectionFactory redisConnectionFactory;
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic", "/every-chat");
 		config.setApplicationDestinationPrefixes("/app");
+
+		// Redis를 메시지 브로커로 사용하도록 설정
+		//중간에 연결이 끊긴 경우 다시 연결을 시도
+		config.enableSimpleBroker("")
+				.setHeartbeatValue(new long[]{0, 30000});
+
 	}
 	/*
 	enableSimpleBroker를 통해 메시지 브로커가 /topic으로 시작하는 주소를 구독한 Subscriber들에게 메시지를 전달하도록 한다.
@@ -37,5 +47,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureClientInboundChannel(ChannelRegistration registration){
 		// jwt 토큰 검증을 위해 생성한 stompHandler를 인터셉터로 지정해준다.
 		registration.interceptors(stompHandler);
+	}
+
+	@Bean
+	public RedisMessageListenerContainer redisMessageListenerContainer(ChatMessageListener chatMessageListener) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(redisConnectionFactory);
+
+		// 채널 구독 설정을 지정 (@EventListener SessionConnectedEvent와 같은 개념)
+		container.addMessageListener(chatMessageListener, new ChannelTopic("/app/chat/addUser:*"));
+		return container;
 	}
 }
