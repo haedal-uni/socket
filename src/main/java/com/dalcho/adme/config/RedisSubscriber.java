7aa4432d0013c08a90c53e0a8c9357cca249378e
soyeon <luora@naver.com>
2023-11-06 22:30:30 +0900

    [update] 필요없는 코드 제거 및 if문 추가 [#88]

--- src/main/java/com/dalcho/adme/config/RedisSubscriber.java
+++ src/main/java/com/dalcho/adme/config/RedisSubscriber.java
@@ -1,6 +1,7 @@
 package com.dalcho.adme.config;
 
 import com.dalcho.adme.dto.ChatMessage;
+import com.dalcho.adme.dto.ChatMessage.MessageType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -55,8 +56,11 @@ public class RedisSubscriber implements MessageListener { // 구독자
                 log.info("구독자 수: " + stringStringMap.get(channel));
             }
 
+            if(chatMessage.getType().equals(MessageType.TALK)){
+                messagingTemplate.convertAndSend(channel, chatMessage);
+            }
             //messagingTemplate.convertAndSend(channel, jsonNode);
-            messagingTemplate.convertAndSend(channel, chatMessage);
+
 
 
         } catch (Exception e){
