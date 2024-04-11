08b77b53906087266e3b73875a9135511fc6bda2
soyeon <luora@naver.com>
2024-04-11 23:26:31 +0900

    [update] 주석 처리 [#108]

--- src/main/java/com/dalcho/adme/config/rabbitmq/ChatMessageProducer.java
+++ src/main/java/com/dalcho/adme/config/rabbitmq/ChatMessageProducer.java
@@ -1,25 +1,25 @@
-package com.dalcho.adme.config.rabbitmq;
-
-import com.dalcho.adme.dto.ChatMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-//@Component
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class ChatMessageProducer {
-
-    private final RabbitTemplate rabbitTemplate;
-
-    @Value("${rabbitmq.exchange.name}")
-    private String exchange;
-
-    public void sendMessage(ChatMessage message, String roomId) {
-        log.info("message send : {}", message);
-        rabbitTemplate.convertAndSend(exchange, roomId, message);
-    }
-}
+//package com.dalcho.adme.config.rabbitmq;
+//
+//import com.dalcho.adme.dto.ChatMessage;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+////@Component
+//@Service
+//@RequiredArgsConstructor
+//@Slf4j
+//public class ChatMessageProducer {
+//
+//    private final RabbitTemplate rabbitTemplate;
+//
+//    @Value("${rabbitmq.exchange.name}")
+//    private String exchange;
+//
+//    public void sendMessage(ChatMessage message, String roomId) {
+//        log.info("message send : {}", message);
+//        rabbitTemplate.convertAndSend(exchange, roomId, message);
+//    }
+//}
