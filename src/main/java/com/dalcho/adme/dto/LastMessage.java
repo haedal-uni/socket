8a1368e5341b83572c34c3ccb23f0983a0d015ea
soyeon <luora@naver.com>
2024-11-13 17:26:42 +0900

    [update] redis cache 저장 관련 일부 코드 수정 [#124]

--- src/main/java/com/dalcho/adme/dto/LastMessage.java
+++ src/main/java/com/dalcho/adme/dto/LastMessage.java
@@ -1,10 +1,12 @@
 package com.dalcho.adme.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LastMessage {
     private String roomId;
     private int adminChat;
@@ -12,6 +14,16 @@ public class LastMessage {
     private String message;
     private String day;
     private String time;
+
+    @Builder
+    public LastMessage(String roomId, int adminChat, int userChat, String message, String day, String time) {
+        this.roomId = roomId;
+        this.adminChat = adminChat;
+        this.userChat = userChat;
+        this.message = message;
+        this.day = day;
+        this.time = time;
+    }
 
     public static LastMessage of(ChatMessage chatMessage, int adminChat, int userChat, String day, String time){
         return LastMessage.builder()
